56f22868360645fcc6a9f925f3a1ae1fc03c72cf
lawsonarichard <51132990+lawsonarichard@users.noreply.github.com>
2019-06-12 11:05:48 -0700

    finished prototype.js no strech

--- assignments/this.js
+++ assignments/this.js
@@ -1,19 +1,30 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1.  Window/Global Object Binding
+* 2.  Implicit Binding
+* 3.  New binding
+* 4.  Explicit binding
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
+let myFunction = function() {
+    console.log(this.x);
+}
+let x = 200;
+myFunction(); 
 // code example for Window Binding
 
 // Principle 2
+let Haku = {
+    name: 'Haku',
+    greeting: function(cat) {
+        console.log("Meow " + cat +", my name is " + this.name);
+    }
+}
+Haku.greeting("world");
 
 // code example for Implicit Binding
 
